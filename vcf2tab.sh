2ad2d1f5ee78e23822c738c78f8591cec55e5796
Marcus Fedarko <mfedarko@ucsd.edu>
2020-05-29 22:12:20 -0700

    Use just one -e option for bcftools query
    Looks like this fixes the problems i was having with filtering
    being inconsistent -- I think specifying things in multiple -e
    options made some samples slip through

--- vcf2tab.sh
+++ vcf2tab.sh
@@ -1,3 +1,3 @@
 CHR_21_VCF_GZ=/datasets/cs284s-sp20-public/1000Genomes/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
 
-bcftools query -e'AF<0.01' -e'AF>0.99' -e'MULTI_ALLELIC=1' -f "[%GT\t]\n" $CHR_21_VCF_GZ | sed 's/0|0/0/g' | sed 's/0|1/1/g' | sed 's/1|0/1/g' | sed 's/1|1/2/g' > ~/plink-182/data/chr_21_genotypes.tab
+bcftools query -e'AF<0.01 || AF>0.99 || MULTI_ALLELIC=1 || VT!="SNP"' -f'[%GT\t]\n' $CHR_21_VCF_GZ | sed 's/0|0/0/g' | sed 's/0|1/1/g' | sed 's/1|0/1/g' | sed 's/1|1/2/g' > ~/plink-182/data/chr_21_genotypes.tab
